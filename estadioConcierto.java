7b6895520e7428c83d9231c7b8aed40b4552d679
cherediamultimedia <98904939+cherediamultimedia@users.noreply.github.com>
2022-02-02 18:27:49 +0100

    Commit de Miguel
    He detectado tres métodos en la interfaz que no se utilizan

--- estadioConcierto.java
+++ estadioConcierto.java
@@ -2,13 +2,9 @@ package interfaces;
 
 public interface estadioConcierto extends estadio {
 
-	public void prepararEscenario();
-	public void prepararAsientos();
-
 	public void abrirEstadio();
 	public void cerrarEstudio();
 	public void dejarPasarGente();
-	public void abrirEstadioConLuces();
 	public void encenderLuces();
 	public void llenarGradas();
 	
