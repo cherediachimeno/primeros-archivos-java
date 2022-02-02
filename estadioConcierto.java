69ef855d9e721d61aeeed43eaf3ff6c27336556a
cherediamultimedia <98904939+cherediamultimedia@users.noreply.github.com>
2022-02-02 18:24:35 +0100

    Commit 3

--- estadioConcierto.java
+++ estadioConcierto.java
@@ -4,5 +4,12 @@ public interface estadioConcierto extends estadio {
 
 	public void prepararEscenario();
 	public void prepararAsientos();
+
+	public void abrirEstadio();
+	public void cerrarEstudio();
+	public void dejarPasarGente();
+	public void abrirEstadioConLuces();
+	public void encenderLuces();
+	public void llenarGradas();
 	
 }
